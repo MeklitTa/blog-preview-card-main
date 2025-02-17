b55f33ae4dbb1287a75cfcd5c66585098cfb5bbc
Meklit <meklittadie@gmail.com>
2025-02-17 15:35:35 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      './public/*.html'
+    ],
+    theme: {
+      extend: {
+        minHeight:{
+        primary:'75vh',
+
+        
+    }
+      },
+    },
+    plugins: [],
+  };
